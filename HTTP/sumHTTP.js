0f1ca80c07c006adc3b5be297073dee8c0264ea1
Hanzala Ahmed <hanzhanzala@gmail.com>
2023-12-14 01:40:46 +0530

    input params

--- HTTP/sumHTTP.js
+++ HTTP/sumHTTP.js
@@ -1,6 +1,14 @@
 const express = require('express');
+var bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+/*
+function middleware1(req, res, next){
+    next();
+}
+app.use(middleware1);
+*/    // for header params
+app.use(bodyParser.json());     // for body params
 
 function calculateSum (n){
     var sum = 0;
@@ -10,27 +18,18 @@ function calculateSum (n){
     return sum;
 }
 
-function welcome (name){
-    return `Welcome ${name} to my page`; 
-}
-
 function handleFirstRequest(req, res){
-    var counter = req.query.counter;
+    // console.log(req.body);
+    var counter = req.body.counter;   // for body params
+    // var counter = req.headers.counter;   // for header params
+    // var counter = req.query.counter;   // for query   params
     var sum = calculateSum(counter);
     console.log(sum);
     var ans = "The sum is " + sum;
     res.send(ans);
 }
 
-function handleSecondRequest(req, res){
-    var name = req.query.name;
-    var ans = welcome(name);
-    console.log(ans);
-    res.send(ans);
-}
-
-app.get('', handleFirstRequest);
-app.get('/welcome', handleSecondRequest);
+app.post('', handleFirstRequest);
 
 function started() {
     console.log(`Example app listening on port ${port}`);
