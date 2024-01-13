149ecfa81845057084dd68254a7ec880daf071be
Hanzala Ahmed <hanzhanzala@gmail.com>
2024-01-13 12:23:39 +0530

    restart

--- web dev/joke.js
+++ web dev/joke.js
@@ -2,3 +2,16 @@ var oneLinerJoke = require('one-liner-joke');
 
 var getRandomJoke = oneLinerJoke.getRandomJoke();
 console.log(getRandomJoke)
+
+var getRandomJokeWithTag = oneLinerJoke.getRandomJokeWithTag('stupid');
+console.log(getRandomJokeWithTag)
+
+var getRandomJoke = oneLinerJoke.getRandomJoke({
+    'exclude_tags': ['dirty', 'racist', 'marriage']
+  });
+console.log(getRandomJoke)
+
+var getRandomJoke = oneLinerJoke.getRandomJokeWithTag('intelligence', {
+    'exclude_tags': ['dirty', 'racist', 'marriage']
+});
+console.log(getRandomJokeWithTag)
